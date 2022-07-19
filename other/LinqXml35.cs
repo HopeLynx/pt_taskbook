ce36c8dc08e50b6392b8526d53018ea756ac6e32
HopeLynx <grigoriy.sizov@gmail.com>
2022-07-19 10:38:38 +0300

    Add files via upload

---
+++ other/LinqXml35.cs
@@ -0,0 +1,29 @@
+// File: "LinqXml35"
+using PT4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Linq;
+
+namespace PT4Tasks
+{
+    public class MyTask : PT
+    {
+        public static void Solve()
+        {
+            Task("LinqXml35");
+
+            string name = GetString();
+            XDocument d = XDocument.Load(name);
+            
+            foreach(var el in d.Root.Elements().Elements()) {
+                el.Add(new XAttribute("child-count", el.Nodes().Count()));
+            }
+
+            d.Save(name);
+
+        }
+    }
+}
